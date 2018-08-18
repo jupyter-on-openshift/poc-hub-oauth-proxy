933bea65157e937ac2ff92e475b869e545d7155b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-08-18 16:44:25 +1000

    Initial files.

---
+++ jupyterhub/.jupyter/jupyterhub_config.py
@@ -0,0 +1,67 @@
+import os
+
+# Enable JupyterLab interface if enabled.
+
+c.Spawner.environment = {}
+
+if os.environ.get('JUPYTERHUB_ENABLE_LAB', 'false').lower() in ['true', 'yes', 'y', '1']:
+    c.Spawner.environment.update(dict(JUPYTER_ENABLE_LAB='true'))
+
+# Setup location for customised template files.
+
+c.JupyterHub.template_paths = ['/opt/app-root/src/templates']
+
+# Populate admin users and use white list from config maps.
+
+if os.path.exists('/opt/app-root/configs/admin_users.txt'):
+    with open('/opt/app-root/configs/admin_users.txt') as fp:
+        content = fp.read().strip()
+        if content:
+            c.Authenticator.admin_users = set(content.split())
+
+if os.path.exists('/opt/app-root/configs/user_whitelist.txt'):
+    with open('/opt/app-root/configs/user_whitelist.txt') as fp:
+        content = fp.read().strip()
+        if content:
+            c.Authenticator.whitelist = set(content.split())
+
+# Provide persistent storage for users notebooks.
+
+c.KubeSpawner.user_storage_pvc_ensure = True
+
+c.KubeSpawner.pvc_name_template = '%s-nb-{username}' % c.KubeSpawner.hub_connect_ip
+c.KubeSpawner.user_storage_capacity = os.environ['NOTEBOOK_VOLUME_SIZE']
+
+c.KubeSpawner.volumes = [
+    {
+        'name': 'data',
+        'persistentVolumeClaim': {
+            'claimName': c.KubeSpawner.pvc_name_template
+        }
+    }
+]
+
+c.KubeSpawner.volume_mounts = [
+    {
+        'name': 'data',
+        'mountPath': '/opt/app-root/src',
+        'subPath': 'notebooks'
+    }
+]
+
+c.Spawner.environment.update(dict(
+    JUPYTER_MASTER_FILES='/opt/app-root/master',
+    JUPYTER_WORKSPACE_NAME='workspace'))
+
+# Setup culling of idle notebooks if timeout parameter is supplied.
+
+idle_timeout = os.environ.get('JUPYTERHUB_IDLE_TIMEOUT')
+
+if idle_timeout and int(idle_timeout):
+    c.JupyterHub.services = [
+        {
+            'name': 'cull-idle',
+            'admin': True,
+            'command': ['cull-idle-servers', '--timeout=%s' % idle_timeout],
+        }
+    ]
