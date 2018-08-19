0338458c9652f9d8a1d162b6ed48996223c51619
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-08-19 18:51:13 +1000

    Debug passing of access token.

--- jupyterhub/.jupyter/jupyterhub_config.py
+++ jupyterhub/.jupyter/jupyterhub_config.py
@@ -25,6 +25,13 @@ if os.path.exists('/opt/app-root/configs/user_whitelist.txt'):
         if content:
             c.Authenticator.whitelist = set(content.split())
 
+# User access. Temporarily trust header sent by the proxy. This
+# needs to be replaced to use the access token to lookup Keycloak
+# and verify the user identity.
+
+#c.JupyterHub.authenticator_class = 'jhub_remote_user_authenticator.remote_user_auth.RemoteUserAuthenticator'
+#c.RemoteUserAuthenticator.header_name = 'X_FORWARDED_USER'
+
 # Provide persistent storage for users notebooks.
 
 c.KubeSpawner.user_storage_pvc_ensure = True
@@ -65,3 +72,72 @@ if idle_timeout and int(idle_timeout):
             'command': ['cull-idle-servers', '--timeout=%s' % idle_timeout],
         }
     ]
+
+
+######
+
+import os
+from jupyterhub.handlers import BaseHandler
+from jupyterhub.auth import Authenticator
+from jupyterhub.auth import LocalAuthenticator
+from jupyterhub.utils import url_path_join
+from tornado import gen, web
+from traitlets import Unicode
+
+
+class RemoteUserLoginHandler(BaseHandler):
+
+    def get(self):
+        header_name = self.authenticator.header_name
+        print('HEADERS', self.request.headers)
+        remote_user = self.request.headers.get(header_name, "")
+        if remote_user == "":
+            raise web.HTTPError(401)
+        else:
+            user = self.user_from_username(remote_user)
+            self.set_login_cookie(user)
+            self.redirect(url_path_join(self.hub.server.base_url, 'home'))
+
+
+class RemoteUserAuthenticator(Authenticator):
+    """
+    Accept the authenticated user name from the REMOTE_USER HTTP header.
+    """
+    header_name = Unicode(
+        default_value='X-Forwarded-User',
+        config=True,
+        help="""HTTP header to inspect for the authenticated username.""")
+
+    def get_handlers(self, app):
+        return [
+            (r'/login', RemoteUserLoginHandler),
+        ]
+
+    @gen.coroutine
+    def authenticate(self, *args):
+        raise NotImplementedError()
+
+
+class RemoteUserLocalAuthenticator(LocalAuthenticator):
+    """
+    Accept the authenticated user name from the REMOTE_USER HTTP header.
+    Derived from LocalAuthenticator for use of features such as adding
+    local accounts through the admin interface.
+    """
+    header_name = Unicode(
+        default_value='X-Forwarded-User',
+        config=True,
+        help="""HTTP header to inspect for the authenticated username.""")
+
+    def get_handlers(self, app):
+        return [
+            (r'/login', RemoteUserLoginHandler),
+        ]
+
+    @gen.coroutine
+    def authenticate(self, *args):
+        raise NotImplementedError()
+
+
+c.JupyterHub.authenticator_class = RemoteUserAuthenticator
+#c.RemoteUserAuthenticator.header_name = 'X_FORWARDED_USER'
--- jupyterhub/start-oauth2-proxy.sh
+++ jupyterhub/start-oauth2-proxy.sh
@@ -12,5 +12,7 @@ exec oauth2_proxy \
   -skip-provider-button \
   -redirect-url=$REDIRECT_URL \
   -oidc-issuer-url=$OIDC_ISSUER_URL \
+  -pass-access-token=true \
+  -pass-user-headers=true \
   -ssl-insecure-skip-verify \
   -cookie-secure=false
